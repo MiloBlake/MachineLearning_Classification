60b9f5eead28c4d9c9312237ca915e3e21e2e51d
MiloBlake <miloblake123@gmail.com>
2025-09-29 17:13:15 +0100

    Initial commit with Datasets + basic SVM and AdaBoost files

---
+++ AdaBoost.py
@@ -0,0 +1,3 @@
+
+test_file = 'Datasets/wildfires_test.csv'
+training_file = 'Datasets/wildfires_training.csv'
---
+++ Support_Vector_Machine.py
@@ -0,0 +1,4 @@
+
+test_file = 'Datasets/wildfires_test.csv'
+training_file = 'Datasets/wildfires_training.csv'
+
